6bfe81ec59ff6d47c3152a51e39e926c63ee414b
ueqt <ueqtxu@gmail.com>
2022-06-26 21:50:30 +0800

    fix

--- all.py
+++ all.py
@@ -5,41 +5,56 @@ import time
 import safaribooks
 import argparse
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from threading import Lock
 
-defaultPath = "../OneDrive - ueqt/safaribooks/_index/"
+currentPath = os.getcwd()
+defaultPath = currentPath + "/../OneDrive - ueqt/safaribooks/_index/"
+lock = Lock()
+thread_count = 64
+result = 0
 
 def dealFile(filename):
-    with open(defaultPath + filename) as json_file:
-        data = json.load(json_file)
-    args = argparse.Namespace()
-    args.bookid = data["archive_id"]
-    args.url = data["url"]
-    args.dir = "../OneDrive - ueqt/safaribooks/"
-    args.no_cookies = None
-    args.log = None
-    args.cred = None
-    args.kindle = None
+    global result
+    with lock:
+        result += 1
+        print('=' * 50)
+        print(result)
+        print(filename)
+        print("begin")
     try:
-        if os.path.isfile("./info_" + args.bookid + ".log"):
+        with open(defaultPath + filename) as json_file:
+            data = json.load(json_file)
+        args = argparse.Namespace()
+        args.bookid = data["archive_id"]
+        args.url = data["url"]
+        args.dir = currentPath + "/../OneDrive - ueqt/safaribooks/"
+        args.no_cookies = None
+        args.log = None
+        args.cred = None
+        args.kindle = None
+        if os.path.isfile(currentPath + "/info_" + args.bookid + ".log"):
             # 有日志
-            if os.stat("./info_" + args.bookid + ".log").st_size == 0 : 
-                os.remove("./info_" + args.bookid + ".log")
+            print('has log')
+            if os.stat(currentPath + "/info_" + args.bookid + ".log").st_size == 0 : 
+                os.remove(currentPath + "/info_" + args.bookid + ".log")
         if not os.path.isfile(args.dir + args.bookid + "/" + args.bookid + ".epub"):
             # 没处理过
+            print('start deal')
             safaribooks.SafariBooks(args)
-    except FileNotFoundError as err:
-        print(args.bookid + " not found")
+    # except FileNotFoundError as err:
+    #     print(args.bookid + " not found")
+        print('deal end')
     except Exception as err:
         print(err)
     print('done: ' + filename)
     return filename
 
 for (dirpath, dirnames, filenames) in os.walk(defaultPath):
-    print(filenames.count())
-    time.sleep(5)
-    with ThreadPoolExecutor(max_workers=16) as t:
+    list_fn = [i for i in filenames]
+    # time.sleep(5)
+    with ThreadPoolExecutor(max_workers=thread_count) as t:
         # begin = time.time()
-        t.map(dealFile, filenames)
+        t.map(dealFile, list_fn)
         # obj_list = []
         # for fn in filenames:
         #     obj = t.submit(dealFile, fn)
@@ -52,6 +67,8 @@ for (dirpath, dirnames, filenames) in os.walk(defaultPath):
 
         # times = time.time() - begin
         # print(times)
+    print(len(list_fn))
+    print(result)
 print('*' * 50)
 print('*' * 50)
 print('*' * 50)
